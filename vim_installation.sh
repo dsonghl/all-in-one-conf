8f7a35766e2cb1f930b8897640bb6892199b247a
Dean Song <dean.song888@gmail.com>
2018-05-13 14:14:53 +0800

    Update vim_installation.sh

--- vim_installation.sh
+++ vim_installation.sh
@@ -8,6 +8,6 @@ mkdir -p ~/.vim/bundle
 
 cd ~/.vim/bundle 
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
